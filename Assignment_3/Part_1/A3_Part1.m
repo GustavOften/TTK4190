9dd0b8841f9bd16b395012f413afd8ec995db659
Paal Thorseth <paalthorseth@gmail.com>
2019-11-11 11:16:28 +0100

    Create Part 1, modify simulink model

---
+++ Assignment_3/Part_1/A3_Part1.m
@@ -0,0 +1,18 @@
+%% Assignment 3 - Part 1
+
+% Add handoutfiles to path
+addpath('/Users/paalthorseth/Documents/Git/TTK4190/Assignment_3/Handouts/Matlab');
+
+
+%% Task 1.2
+
+%% Task 1.3
+
+%% Task 1.4
+
+% Initial conditions (might be unnecessary, see run.m)
+u0      = 6.63;
+v0      = 0;
+psi0    = 0;
+r0      = 0;
+n_c0    = 7.3;
