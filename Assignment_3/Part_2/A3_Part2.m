c42ec065f1795e97e9e4088fbc98ee2ac4fb7e5d
Paal Thorseth <paalthorseth@gmail.com>
2019-11-13 14:37:05 +0100

    Add Part 2

---
+++ Assignment_3/Part_2/A3_Part2.m
@@ -0,0 +1,23 @@
+%% Assignment 3 - Part 2
+
+% Add handoutfiles and MSS-master to path
+addpath('/Users/paalthorseth/Documents/Git/TTK4190/Assignment_3/Handouts/Matlab');
+addpath('/Users/paalthorseth/Documents/Git/TTK4190/MSS-master');
+
+%% Initial conditions
+
+p0=[1000,700]';     % Initial position (NED)
+v0=[6.63 0]';       % Initial velocity (body)
+psi0=0;             % Inital yaw angle
+r0=0;               % Inital yaw rate
+c=0;                % Current on (1)/off (0)
+
+%% Task 2.1
+
+
+
+%% Task 2.2
+
+
+
+
