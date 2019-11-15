fffaa899872e9f2a134f47489797d24da383a280
Paal Thorseth <paalthorseth@gmail.com>
2019-11-15 17:58:06 +0100

    Add code for simulating path following with and without crab angle, add code for plotting

--- Assignment_3/Part_2/A3_Part2.m
+++ Assignment_3/Part_2/A3_Part2.m
@@ -9,14 +9,13 @@ addpath('../Common')
 
 dummy_values;
 
-
 %% Initial conditions
 
 p0=[1000,700]';     % Initial position (NED)
 v0=[6.63 0]';       % Initial velocity (body)
 psi0=60*pi/180;     % Inital yaw angle
 r0=0;               % Inital yaw rate
-c=0;                % Current on (1)/off (0)
+c=1;                % Current on (1)/off (0)
 delta_c=0;          % Constant reference, not used for this part
 
 %% Heading autopilot
@@ -24,7 +23,6 @@ delta_c=0;          % Constant reference, not used for this part
 % Model parameters
 T       = 130.7054;
 K       = -0.0544;
-
 
 % Heading control parameters, PID
 zeta    = 1;        % Damping ratio
@@ -39,13 +37,13 @@ K_d_psi = 1/K * (2*zeta*omega_n*T - 1);
 % Speed control parameters, Feedback
 k_reg   = 0.3445;
 r_reg   = 1.3628;
-
-%% Task 2.1
+                   
+%% Task 2.2
 
 % Activate both controllers
 dc_mode  = DC_MODE.CONTROLLER;
 nc_mode  = NC_MODE.CONTROLLER;
-ref_mode = REF_MODE.PATH_FOLLOWING;
+ref_mode = REF_MODE.PATH_FOLLOWING_1;
 
 % Simulate system
 tstart = 0;        % Sim start time
@@ -55,60 +53,40 @@ tsamp  = 10;       % Sampling time for how often states are stored.
                    
 sim MSFartoystyring 
 
+%% Task 2.6
+
+% Activate both controllers
+dc_mode  = DC_MODE.CONTROLLER;
+nc_mode  = NC_MODE.CONTROLLER;
+ref_mode = REF_MODE.PATH_FOLLOWING_2;
+
+% Simulate system
+tstart = 0;        % Sim start time
+tstop  = 10000;    % Sim stop time
+tsamp  = 10;       % Sampling time for how often states are stored. 
+                   %     (NOT ODE solver time step)
+                   
+sim MSFartoystyring 
+
+%% Plot closed-loop behaviour
+
 pathplotter(p(:,1), p(:,2), psi, tsamp, 1, tstart, tstop, 0, WP);
 
+%% Task 2.4 and 2.6
+
+% Course
+chi     = psi + beta;
+
 % Plot
-
-% Heading
 figure('rend','painters','pos',[10 10 750 400]);
-
-subplot(1,2,1);
 hold on;
-plot(t, psi*180/pi,'b-');
-plot(t, psi_d*180/pi, 'r-.');
+plot(t, psi*180/pi,'b-.');
+plot(t, chi*180/pi,'g-');
+plot(t, chi_d*180/pi, 'r-');
+plot(t, beta*180/pi, 'm-');
 grid on;
 hold off;
-title('Heading $\psi$ versus Heading reference $\psi_d$', 'Interpreter', 'latex');
+title('Heading $\psi$ vs Course $\chi$ vs Course reference $\chi_d$ vs crab angle $\beta$', 'Interpreter', 'latex');
 xlabel('Time [s]', 'Interpreter', 'latex');
 ylabel('Angle [deg]', 'Interpreter', 'latex');
-legend('$\psi$', '$\psi_d$', 'Interpreter', 'latex');
-
-subplot(1,2,2);
-hold on;
-plot(t, tilde_psi*180/pi,'k-');
-grid on;
-hold off;
-title('Heading error $\tilde{\psi}$', 'Interpreter', 'latex');
-xlabel('Time [s]', 'Interpreter', 'latex');
-ylabel('Angle [deg]', 'Interpreter', 'latex');
-legend('$\tilde{\psi}$', 'Interpreter', 'latex');
-
-% Surge
-figure('rend','painters','pos',[10 10 750 400]);
-subplot(1,2,1);
-hold on;
-plot(t, v(:,1),'b-');
-plot(t, u_d, 'r--');
-grid on;
-hold off;
-title('Surge performance with current, PID controllers', ...
-    'Interpreter', 'latex');
-xlabel('Time [s]', 'Interpreter', 'latex');
-ylabel('Velocity [m/s]', 'Interpreter', 'latex');
-legend('$u$', '$u_d$', 'Interpreter', 'latex');
-
-subplot(1,2,2);
-plot(t, u_tilde, 'm');
-grid on;
-hold off;
-title('Surge error, PID controllers', ...
-    'Interpreter', 'latex');
-xlabel('Time [s]', 'Interpreter', 'latex');
-ylabel('Velocity [m/s]', 'Interpreter', 'latex');
-legend('$\tilde{u}$', 'Interpreter', 'latex');
-                   
-%% Task 2.2
-
-
-
-
+legend('$\psi$', '$\chi$','$\chi_d$','$\beta$', 'Interpreter', 'latex');
