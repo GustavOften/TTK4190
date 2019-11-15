a68eb3f329d4867d814fce4cbf7610f24987995b
Paal Thorseth <paalthorseth@gmail.com>
2019-11-15 17:57:32 +0100

    Major simulink update

--- Assignment_3/Common/REF_MODE.m
+++ Assignment_3/Common/REF_MODE.m
@@ -1,9 +1,10 @@
 classdef REF_MODE < Simulink.IntEnumType
     %REF_MODE Summary of this class goes here
     enumeration
-        PATH_FOLLOWING  (1),
-        TARGET_TRACKING (2),
-        MANUAL_REF      (3)
+        PATH_FOLLOWING_1    (1),
+        PATH_FOLLOWING_2    (2),
+        TARGET_TRACKING     (3),
+        MANUAL_REF          (4)
     end
 end
 
